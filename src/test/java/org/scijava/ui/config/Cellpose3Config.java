53a3f88e175a74894f6d9bb6746c32134b134fb4
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-08-05 13:27:35 +0200

    Extract the Cellpose3Config class for JUnit tests.

---
+++ src/test/java/org/scijava/ui/config/Cellpose3Config.java
@@ -0,0 +1,226 @@
+package org.scijava.ui.config;
+
+import org.scijava.ui.config.Parameters.BooleanParam;
+import org.scijava.ui.config.Parameters.DoubleParam;
+import org.scijava.ui.config.Parameters.EnumParam;
+import org.scijava.ui.config.Parameters.IntParam;
+import org.scijava.ui.config.Parameters.PathParam;
+
+public class Cellpose3Config extends Configurator
+{
+	final EnumParam< Cellpose3BuiltinModels > builtinModel;
+
+	private final PathParam customModel;
+
+	final SelectableParameters builtinOrCustom;
+
+	final DoubleParam diameter;
+
+	final IntParam chan1;
+
+	final IntParam chan2;
+
+	private final DoubleParam flowThreshold;
+
+	private final DoubleParam cellprobThreshold;
+
+	private final IntParam minSize;
+
+	private final BooleanParam exportROIs;
+
+	private final BooleanParam exportLabels;
+
+	private final BooleanParam exportFlows;
+
+	public Cellpose3Config( final Integer nChannels, final double pixelSize, final String units )
+	{
+		super( "Cellpose 3", "https://imagej.net/plugins/cellpose-appose#usage" );
+
+		// Choice among an enum.
+		this.builtinModel = addEnumParameter( Cellpose3BuiltinModels.class )
+				.key( "BUILTIN_MODEL" )
+				.defaultValue( Cellpose3BuiltinModels.CYTO3 )
+				.name( "Builtin model" )
+				.help( "https://cellpose.readthedocs.io/en/v3.1.1.1/models.html#full-built-in-models" )
+				.get();
+
+		// File path.
+		this.customModel = addPathParameter()
+				.key( "CUSTOM_MODEL_PATH" )
+				.defaultValue( "" ) // Better than null.
+				.name( "Path to custom model" )
+				.help( "Path to a custom Cellpose 3 model. " )
+				.get();
+
+		// One or the other, but not both.
+		this.builtinOrCustom = addSelectableParameters()
+				.key( "BUILTIN_OR_CUSTOM" )
+				.add( builtinModel )
+				.add( customModel )
+				.get();
+
+		// Channels, two int params.
+		this.chan1 = addIntParameter()
+				.key( "CHAN1" )
+				.name( "Main channel" )
+				.help( "The main channel to segment. Select 0 to use a grayscale blend of all channels." )
+				.defaultValue( 1 )
+				.min( 0 )
+				.max( nChannels )
+				.get();
+		this.chan2 = addIntParameter()
+				.key( "CHAN2" )
+				.name( "Optional channel" )
+				.help( "The second channel to segment. Select 0 to skip using a second channel." )
+				.defaultValue( 0 )
+				.min( 0 )
+				.max( nChannels )
+				.get();
+
+		// Diameter param is in pixel, but we want to display it in physical
+		// units. So we set a translator that converts between the two.
+
+		this.diameter = addDoubleParameter()
+				.key( "DIAMETER" )
+				.name( "Diameter" )
+				.help( "<html>Estimated diameter of objects, in physical units "
+						+ "(stored in pixel size internally). " +
+						"Set to 0 to let Cellpose estimate it automatically.</html>" )
+				.units( units )
+				.defaultValue( 30. )
+				.min( 0. ) // But no max
+				.get();
+
+		setDisplayTranslator( diameter, d -> d * pixelSize, d -> d / pixelSize );
+
+		/*
+		 * Advanced parameters.
+		 */
+
+		this.flowThreshold = addDoubleParameter()
+				.key( "FLOW_THRESHOLD" )
+				.name( "Flow threshold" )
+				.help( "<html>Threshold for flow error filtering. Lower = more masks (permissive), Higher = fewer masks (strict).</html>" )
+				.defaultValue( 0.4 )
+				.min( 0. )
+				.max( 3. )
+				.get();
+
+		this.cellprobThreshold = addDoubleParameter()
+				.key( "CELPROB_THRESHOLD" )
+				.name( "Cell probability threshold" )
+				.help( "<html>Threshold for cell probability. Increase to filter low-confidence detections.</html>" )
+				.defaultValue( 0.0 )
+				.min( -6. )
+				.max( 6. )
+				.get();
+
+		this.minSize = addIntParameter()
+				.key( "MIN_SIZE" )
+				.name( "Minimum size" )
+				.help( "Objects smaller than this are removed." )
+				.defaultValue( 15 )
+				.min( 0 )
+				.units( "pixels" )
+				.get();
+
+		addGroup( "Advanced parameters" )
+				.add( flowThreshold )
+				.add( cellprobThreshold )
+				.add( minSize )
+				.collapsed( true )
+				.get();
+
+		/*
+		 * Export group.
+		 */
+
+		this.exportROIs = addBooleanParameter()
+				.key( "EXPORT_ROIS" )
+				.name( "Export ROIs" )
+				.help( "If set, ROIs will be computed from the labels output and added to the input image." )
+				.defaultValue( true )
+				.get();
+
+		this.exportLabels = addBooleanParameter()
+				.key( "EXPORT_LABELS" )
+				.name( "Export label image" )
+				.help( "If set, the label image will be shown." )
+				.defaultValue( false )
+				.get();
+
+		this.exportFlows = addBooleanParameter()
+				.key( "EXPORT_FLOWS" )
+				.name( "Export flows" )
+				.help( "If set, the Cellpose flows will be shown as a 3-channel image" )
+				.defaultValue( false )
+				.get();
+
+		addGroup( "Export options" )
+				.add( exportROIs )
+				.add( exportLabels )
+				.add( exportFlows )
+				.collapsed( false )
+				.get();
+	}
+
+	public EnumParam< Cellpose3BuiltinModels > builtinModel()
+	{
+		return builtinModel;
+	}
+
+	public PathParam customModel()
+	{
+		return customModel;
+	}
+
+	public SelectableParameters builtinOrCustom()
+	{
+		return builtinOrCustom;
+	}
+
+	public DoubleParam diameter()
+	{
+		return diameter;
+	}
+
+	public IntParam chan1()
+	{
+		return chan1;
+	}
+
+	public IntParam chan2()
+	{
+		return chan2;
+	}
+
+	public DoubleParam flowThreshold()
+	{
+		return flowThreshold;
+	}
+
+	public DoubleParam cellprobThreshold()
+	{
+		return cellprobThreshold;
+	}
+
+	public IntParam minSize()
+	{
+		return minSize;
+	}
+
+	public BooleanParam exportROIs()
+	{
+		return exportROIs;
+	}
+
+	public BooleanParam exportLabels()
+	{
+		return exportLabels;
+	}
+
+	public BooleanParam exportFlows()
+	{
+		return exportFlows;
+	}
+}
