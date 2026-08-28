b85f4250b92b3cb4bf85ad3a1ef8ec42c63ad64b
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-08-28 17:45:35 +0200

    Clamp values when setting them instead of throwing an error.
    Otherwise this causes unwanted crashes when e.g. using
    a 'last settings used' in TrackMate.

--- src/main/java/org/scijava/ui/config/Parameters.java
+++ src/main/java/org/scijava/ui/config/Parameters.java
@@ -6,18 +6,18 @@
  * %%
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * 3. Neither the name of the Institut Pasteur nor the names of its contributors
  *    may be used to endorse or promote products derived from this software without
  *    specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -551,9 +551,9 @@ public class Parameters
 		public void set( final O value )
 		{
 			if ( hasMax() && value.compareTo( max ) > 0 )
-				throw new IllegalArgumentException( "Value " + value + " is higher than the maximum of " + max + "." );
+				super.set( max );
 			else if ( hasMin() && value.compareTo( min ) < 0 )
-				throw new IllegalArgumentException( "Value " + value + " is lower than the minimum of " + min + "." );
+				super.set( min );
 			else
 				super.set( value );
 		}
